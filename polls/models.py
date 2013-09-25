1d00fcfe27a3c768f2e4e2e0a47ee76619389fe9
mentas <mentas777@gmail.com>
2013-09-26 01:21:59 +0400

    add test and update template

--- polls/models.py
+++ polls/models.py
@@ -1,8 +1,15 @@
+import datetime
 from django.db import models
+from django.utils import timezone
+
 
 class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <  now
 
 
 class Choice(models.Model):
